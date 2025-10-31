a18b98b5e0dbe6ecc56318d266748286ab87506c
Hoyang Tsai <dev@hoyang.dev>
2025-10-31 17:03:05 +0800

    Add safari extension

---
+++ JSON Formatter/iOS (App)/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  iOS (App)
+//
+//  Created by 蔡和洋 on 2025/10/31.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
